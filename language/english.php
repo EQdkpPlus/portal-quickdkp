b2d69930886355e6943ebee8be8b346e9f729873
godmod <godmod@eqdkp-plus.eu>
2012-12-17 14:22:05 +0000

    quickdkp module: create tag, use trunk folder

---
+++ language/english.php
@@ -0,0 +1,33 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2012-01-07 19:20:49 +0100 (Sa, 07. Jan 2012) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: Godmod $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 11607 $
+ * 
+ * $Id: english.php 11607 2012-01-07 18:20:49Z Godmod $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$lang = array(
+	'quickdkp'			=> 'My characters',
+	'quickdkp_name'		=> 'Own characters',
+	'quickdkp_desc'		=> 'Shows an overview of your points for all your characters.',
+	'quickdkp_char'		=> 'No characters connected to your account.',
+	'pm_quickdkp_mdkps' => 'Select MultiDKP Pool',
+	'pm_quickdkp_tooltip'	=> 'Show Tooltip with extended Point-Information.',
+	'lm_page_quickdkp'	=> 'Quick DKP',
+	'lm_hptt_quickdkp_tooltip' => 'Quick DKP Tooltip',
+);
+?>
